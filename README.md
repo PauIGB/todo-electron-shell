593f69d41002c2b2cb87e20db5c05f5d27639b29
Pavel Karpov <pavel.karpov@myoffice.team>
2024-08-30 09:25:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Electron desktop shell for TODO app
+
+## Install dependencies
+
+`npm i`
+
+## Development
+
+`npm start`
---
+++ main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+
+const __dirname = path.resolve()
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    height: 800,
+    width: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  // for MacOS when app is running but without opened windows
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
